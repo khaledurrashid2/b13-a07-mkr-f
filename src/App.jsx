847f6e41b47a5507e25160942e6a0fc73f782391
khaledurrashid2@gmail.com <khaledurrashid2@gmail.com>
2026-04-17 10:02:40 +0600

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { AppProvider } from "./context/AppContext";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import FriendDetails from "./pages/FriendDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
+function App() {
   return (
-    <div className="p-10">
-      <button className="btn btn-primary">Hello DaisyUI</button>
-    </div>
+    <AppProvider>
+      <BrowserRouter>
+        <div style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#f5f7f4",
+        }}>
+          <Navbar />
+
+          <main style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/"           element={<Home />} />
+              <Route path="/friend/:id" element={<FriendDetails />} />
+              <Route path="/timeline"   element={<Timeline />} />
+              <Route path="/stats"      element={<Stats />} />
+              <Route path="*"           element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+
+        <Toaster toastOptions={{ style: { zIndex: 9999 } }} />
+      </BrowserRouter>
+    </AppProvider>
   );
 }
+
+export default App;
